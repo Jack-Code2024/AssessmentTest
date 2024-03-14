c433e4cfdb7501b283708760ad6522d0a403707b
Jack-Code2024 <163379115+Jack-Code2024@users.noreply.github.com>
2024-03-14 13:04:14 +0800

    Add files via upload

---
+++ UnitTest/Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+  static void Main(string[] args)
+  {
+    ConvertServiceTests tests = new ConvertServiceTests();
+    tests.ConvertNumberToText_Test();
+    Console.ReadLine();
+  }
+}
